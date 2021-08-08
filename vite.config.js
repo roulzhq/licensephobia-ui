cab7619007d73e524f390d62141c5305cf9b79a5
Janis Jansen <janis.jansen@gmx.de>
2021-08-08 20:14:22 +0200

    Added base app

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { defineConfig } from 'vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [svelte()]
+});
