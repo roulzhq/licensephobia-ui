172f08612c8d13498c5257a5a88e55fea71086db
ixexel <xelexpro@gmail.com>
2021-08-11 02:46:05 +0200

    Added File Upload (not Tested)
    Not tested since backend not yet working

--- src/lib/components/Package-Upload.svelte
+++ src/lib/components/Package-Upload.svelte
@@ -1,1 +1,34 @@
-<input type="file">
+<script>
+
+    let files;
+    let jsonFile = null;
+
+    const uploadClick = () => {
+      
+      const formData = new FormData();
+      formData.append('jsonFile', files[0]);
+      const upload = fetch('url', {
+          method: 'POST',
+          body: formData
+      }).then((response) => response.json()).then((result) => {
+          console.log('Success: ', result);
+      })
+        .catch((error) => {
+            console.error('Error: ', error);
+        });
+    };
+
+
+</script>
+
+    <input type="file" id="uploadJson" name="uploadJson" bind:files>
+
+    {#if jsonFile && files[0]}
+        <p>{files[0].name}</p>
+    {/if}
+
+    <!-- {#if value} -->
+        <button on:click={uploadClick}>Submit</button>
+    <!-- {:else} -->
+        <button on:click={uploadClick} disabled>Submit</button>
+    <!-- {/if} -->
