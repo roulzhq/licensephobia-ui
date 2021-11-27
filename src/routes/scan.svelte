43bff95c5d647ca3175d5382f438ad399df4a25a
Janis Jansen <janis.jansen@gmx.de>
2021-11-28 00:52:51 +0100

    Minor style changes

--- src/routes/scan.svelte
+++ src/routes/scan.svelte
@@ -55,7 +55,7 @@
 			grid-template-columns: repeat(3, 1fr);
 			grid-auto-rows: 120px;
 			gap: 10px;
-			padding: 10px;
+			padding: 0 10px 10px 10px;
 			margin-top: 15px;
 			overflow-x: hidden;
 			overflow-y: auto;
