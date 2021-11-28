c6eeb2680c7782169be1bb19cd2b073fbff8bd7e
ixexel <xelexpro@gmail.com>
2021-11-28 15:09:57 +0100

    Updated types.ts

--- src/types.ts
+++ src/types.ts
@@ -1,21 +1,45 @@
 export interface PackageResult {
 	found: boolean;
 	name: string;
-	id: string;
-	description: string;
-	url: string;
-	version: {
-		used: string;
-		latest: string;
-	};
-	license: {
-		found: boolean;
-		known: boolean;
-		type: LicenseString;
-	};
+	version: string;
+	package: Package;
+	author: string;
+	homepage: string;
 }
 
 export type LicenseString = 'MIT' | 'GNU';
+
+export interface WebsocketMessage {
+	type: 'package' | 'summary';
+	data: 'PackageResult' | 'SummaryResult';
+}
+
+export interface Package {
+	id: string;
+	name: string;
+	description: string;
+	latestVersion: string;
+	license: License;
+}
+
+export interface PackageDetailResult {
+	found: boolean;
+	package: {
+		id: string;
+		name: string;
+		description: string;
+		latestVersion: string;
+		license: License;
+		author: string;
+		homepage: string;
+	};
+}
+
+export interface License {
+	found: boolean;
+	known: boolean;
+	type: LicenseString;
+}
 
 export interface SummaryResult {
 	conditions: SummaryConditions;
